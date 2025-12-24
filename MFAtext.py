d4e89d8ef327588a48b3f6066e6faef3e35f4d59
FAT4L-ERR0R <altmailid2@gmail.com>
2025-12-24 14:22:32 +0900

    Transcripts and transcript sorter

--- MFAtext.py
+++ MFAtext.py
@@ -18,3 +18,10 @@ with open(f"S{x}Compiled.txt", "w", encoding="utf-8") as f:
 
 
 
+Z:\FluentifyAI\CodeBase>git fetch origin
+no such identity: C:/Users/USER/.ssh/DEPLOY_KEY_FILENAME: No such file or directory
+git@github.com: Permission denied (publickey).
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
