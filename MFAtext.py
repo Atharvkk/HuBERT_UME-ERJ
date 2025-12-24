b7829ee07c31c12c92a2e4c6344656571d20a147
Atharvkk <kulatharv20@gmail.com>
2025-12-24 13:14:05 +0900

    Initial commit

---
+++ MFAtext.py
@@ -0,0 +1,20 @@
+import re
+from PyPDF2 import PdfReader as pd
+
+full_text = ""
+
+pattern = re.compile(r'[^a-zA-Z0-9_\s.]+')
+
+for x in range(1,9):
+    reader = pd(fr"Z:\FluentifyAI\dataset\UME-ERJ_1\doc\JErecord\s{x}_record.pdf")
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            cleaned_text = pattern.sub(' ', page_text) 
+            full_text += cleaned_text + "\n"
+
+with open(f"S{x}Compiled.txt", "w", encoding="utf-8") as f:
+    f.write(full_text)
+
+
+
